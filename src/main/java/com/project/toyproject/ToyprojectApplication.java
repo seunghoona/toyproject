bf8610c8ea9ef80b76099fe91ff704bcf50cf64f
naseu <naseunghoo@naver.com>
2021-09-07 12:07:33 +0900

    setting new

--- src/main/java/com/project/toyproject/ToyprojectApplication.java
+++ src/main/java/com/project/toyproject/ToyprojectApplication.java
@@ -1,17 +1,23 @@
 package com.project.toyproject;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RequiredArgsConstructor
 public class ToyprojectApplication {
+
+    private final EntityManager entityManager;
 
     public static void main(String[] args) {
         SpringApplication.run(ToyprojectApplication.class, args);
@@ -21,6 +27,11 @@ public class ToyprojectApplication {
     @Bean
     public AuditorAware<String> auditorProvider(){
         return () -> Optional.of(UUID.randomUUID().toString());
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
     }
 
 }
